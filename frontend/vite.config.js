4a3c6abc6d9fb5a8c371ab69375e24ca69c36e39
Anmol SIngh <anmol210202@gmail.com>
2025-04-17 23:07:28 +0530

    vercel.json fix --- attempt 7

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,14 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/configure/',
   build: {
     outDir: '../frontend/dist',
     emptyOutDir: true,
     assetsDir: 'assets',
-    manifest: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    }
+    manifest: true
   }
 })
